740955378e24638d2dd0768e5bc65817b246c189
Tim Cumming <timcumming123@gmail.com>
2013-06-13 21:29:24 +0100

    Change version number

--- nesi.py
+++ nesi.py
@@ -414,7 +414,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
         #info.SetIcon(wx.Icon('hunter.png', wx.BITMAP_TYPE_PNG))
         info.SetName('Nova Echo Science & Industry')
-        info.SetVersion('0.1')
+        info.SetVersion('0.0.1')
         info.SetDescription(description)
         info.SetCopyright('(C) 2013 Tim Cumming')
         info.SetWebSite('https://github.com/EluOne/Nesi')
