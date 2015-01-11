b7cb0dd94a06c028432a0c2fb2537f4f98249274
Tim Cumming <timcumming123@gmail.com>
2015-01-11 23:50:30 +0000

    Start of work to use kivy as gui framework.

---
+++ nesi/app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+"""Nova Echo Science & Industry"""
+# Copyright (C) 2015  Tim Cumming
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Tim Cumming aka Elusive One
+# Created: 11/01/15
+
+import kivy
+
+kivy.require('1.8.0')
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.anchorlayout import AnchorLayout
+
+
+class RootWidget(GridLayout):
+    '''This the class representing your root widget.
+       By default it is inherited from BoxLayout,
+       you can use any other layout/widget depending on your usage.
+    '''
+    pass
+
+
+class NesiApp(App):
+    title = 'Nesi'
+    '''This is the main class of your app.
+       Define any app wide entities here.
+       This class can be accessed anywhere inside the kivy app as,
+       in python::
+
+         app = App.get_running_app()
+         print (app.title)
+
+       in kv language::
+
+         on_release: print(app.title)
+       Name of the .kv file that is auto-loaded is derived from the name of this cass::
+
+         MainApp = main.kv
+         MainClass = mainclass.kv
+
+       The App part is auto removed and the whole name is lowercased.
+    '''
+
+    def build(self):
+        '''Your app will be build from here.
+           Return your root widget here.
+        '''
+        return RootWidget()
+
+    def on_pause(self):
+        '''This is necessary to allow your app to be paused on mobile os.
+           refer http://kivy.org/docs/api-kivy.app.html#pause-mode .
+        '''
+        return True
