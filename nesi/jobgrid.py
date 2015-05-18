4c18dfc471941e12d0c599e9e60f6d9cb7fb9429
Tim Cumming <timcumming123@gmail.com>
2015-05-18 22:53:33 +0100

    Use widget init to check for existing cached data for initial display.

--- nesi/jobgrid.py
+++ nesi/jobgrid.py
@@ -26,6 +26,26 @@ import config
 
 
 class JobGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(JobGrid, self).__init__(**kwargs)
+        self.add_widget(Label(font_size=12, text='State'))
+        self.add_widget(Label(font_size=12, text='Activity'))
+        self.add_widget(Label(font_size=12, text='Type'))
+        self.add_widget(Label(font_size=12, text='Location'))
+        self.add_widget(Label(font_size=12, text='Installer'))
+        self.add_widget(Label(font_size=12, text='Install Date'))
+        self.add_widget(Label(font_size=12, text='End Date'))
+        if config.jobRows:
+            numItems = list(range(len(config.jobRows)))
+            for r in numItems:
+                self.add_widget(Label(font_size=12, id=str(config.jobRows[r].jobID), text=str(config.jobRows[r].completedStatus)))
+                self.add_widget(Label(font_size=12, id=str(config.jobRows[r].jobID), text=str(config.jobRows[r].activityID)))
+                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installedItemTypeID)))
+                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installedInSolarSystemID)))
+                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installerID)))
+                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installTime)))
+                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].endProductionTime)))
+
     def add_row(self, completedStatus, activityID, installedItemTypeID, installedInSolarSystemID,
                 installerID, installTime, endProductionTime):
         print('Adding Rows')
@@ -38,11 +58,21 @@ class JobGrid(GridLayout):
         self.add_widget(Label(font_size=12, text=endProductionTime))
 
     def update_rows(self):
+        # At present clear the grid and start fresh.
+        self.clear_widgets()
+        self.add_widget(Label(font_size=12, text='State'))
+        self.add_widget(Label(font_size=12, text='Activity'))
+        self.add_widget(Label(font_size=12, text='Type'))
+        self.add_widget(Label(font_size=12, text='Location'))
+        self.add_widget(Label(font_size=12, text='Installer'))
+        self.add_widget(Label(font_size=12, text='Install Date'))
+        self.add_widget(Label(font_size=12, text='End Date'))
+
         if config.jobRows:
             numItems = list(range(len(config.jobRows)))
             for r in numItems:
-                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].completedStatus)))
-                self.add_widget(Label(font_size=12, text=str(config.jobRows[r].activityID)))
+                self.add_widget(Label(font_size=12, id=str(config.jobRows[r].jobID), text=str(config.jobRows[r].completedStatus)))
+                self.add_widget(Label(font_size=12, id=str(config.jobRows[r].jobID), text=str(config.jobRows[r].activityID)))
                 self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installedItemTypeID)))
                 self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installedInSolarSystemID)))
                 self.add_widget(Label(font_size=12, text=str(config.jobRows[r].installerID)))
